c8e59fa1e886adf469a05ad9e5e16b9e0c8c0f33
SandisD1 <sandisdreimanis2@gmail.com>
2023-02-15 15:51:13 +0200

    update

--- src/main/java/io/linkshortener/config/DatasourceConfiguration.java
+++ src/main/java/io/linkshortener/config/DatasourceConfiguration.java
@@ -17,19 +17,8 @@ public class DatasourceConfiguration {
         return DataSourceBuilder.create()
                 .driverClassName("org.h2.Driver")
                 .url("jdbc:h2:mem:test")
-                .username("sa")
-                .password("sa")
-                .build();
-    }
-
-    @Bean
-    @ConditionalOnProperty(prefix = "link-shortener", name = "database", havingValue = "postgres")
-    public DataSource getDatabaseDataSourcePostgresql() {
-        return DataSourceBuilder.create()
-                .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://localhost:5432/postgres")
-                .username("postgres")
-                .password("docker")
+                .username("name")
+                .password("password")
                 .build();
     }
 }
